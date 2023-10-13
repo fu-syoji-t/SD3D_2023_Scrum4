357f36a0a512f99f9f028fa0cd8a42a3eebfd43f
sota94 <2101212@s.asojuku.ac.jp>
2023-10-13 10:07:12 +0900

    kousinn

--- 07コード類/02バックエンド/05_新規投稿作成.php
+++ 07コード類/02バックエンド/05_新規投稿作成.php
@@ -12,9 +12,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.4/css/all.css">
     <link href="../01フロントエンド/css/nakai.css" rel="stylesheet" type="text/css">
-    <link href="css/yamane.css" rel="stylesheet" type="text/css">
-    <link href="css/yamanishi.css" rel="stylesheet" type="text/css">
-    <link href="css/tomoyuki.css" rel="stylesheet" type="text/css">
+    <link href="../01フロントエンド/css/yamane.css" rel="stylesheet" type="text/css">
+    <link href="../01フロントエンド/css/yamanishi.css" rel="stylesheet" type="text/css">
+    <link href="../01フロントエンド/css/tomoyuki.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
 </head>
 <style>
