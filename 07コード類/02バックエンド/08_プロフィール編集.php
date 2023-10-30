e69e9323e72b549f527b3bc97deb0ba1df1a7c3e
ymnrio <2101213@s.asojuku.ac.jp>
2023-10-30 15:02:24 +0900

    更新

--- 07コード類/02バックエンド/08_プロフィール編集.php
+++ 07コード類/02バックエンド/08_プロフィール編集.php
@@ -19,74 +19,51 @@
 </head>
 <style>
     input[type="file"] {
-      display: none;
+        display: none;
     }
 </style>
 
-<body>
-
+<body class="profileupdate_ymn">
     <form action="profile_update.php" method="post" enctype="multipart/form-data">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-md-3 back_pink_yamani" style="height:100vh"></div>
-                    <div class="col-md-6">
-                        <div class="row">
-                            <div class="col-md-5"></div>
-                                <div class="col-md-1">
-                                    <!--<img class="image_middle" src="img/pink.png" style="margin-top:25px; ">-->
-                                    <?php
-                                        if (!empty($_SESSION['user']['icon']) || isset($_SESSION['user']['icon'])) { //設定している場合
-      
-                                            $base64_image = base64_encode($_SESSION['user']['icon']);
-      
-                                            echo '<br>' . '<img class="image_middle" width="250"src="data:image/jpeg;base64,' .  $base64_image . '" />　';
-      
-                                        } else { //設定してない場合
-      
-                                            echo '<img class="image_middle" src="img/pink.png">　';
-                                        }
-                                    ?>
-
-                                    <label class="btn container-fluid color_white_yamani aikn_ys start_0_ys border border-dark">
-                                        <input type="file" name="file" accept="image/*">
-                                        <p class="p_pusu_ys">＋</p>
-                                    </label>
-
-                                </div>
-                                <div class="col-md-6 usiro_ys">
-                                <!--<p style="margin-left:30px;margin-top: 45px;"><?php //echo $_SESSION['user']['name'] 
-                                                                    ?></p>-->
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-5"></div>
+                        <div class="col-md-1">
+                            <div style="text-align:center"><img class="iconimage_ymn" src="img/やますたぐるめcolor.png"
+                                    style="margin-top:25px; "></div>
+                            <div style="margin-left: 200px; margin-top: -20px;">
+                                <label class="btn container-fluid icon_ymn border border-dark">
+                                    <input type="file" name="file" accept="image/*">
+                                    <p class="iconmedia_ymn">＋</p>
+                                </label>
                             </div>
-                        </div><br>
-                        <p style="margin-left: 30px;">名前</p>
-                        <input type="text" class="form-control" name="username" required value="<?php echo $_SESSION['user']['name']; ?>" style="width:570px; margin: 0 auto; border-color:#FBA8B8;border-width:3px;">
-    
-                        <textarea class="sayu_ys form-control alert-light magin40_yamanisi " 
-                            style="width: 90%;height: 30px;text-align:left;border:none;overflow-wrap: break-word;margin-top: 70px;" 
-                            name="introduction" id="txt1" maxlength="200" placeholder="自己紹介"><?php echo $_SESSION['user']['introduction']; ?></textarea>
-                        <hr class="sayu_ys">
-                        <div style="text-align: right;margin-right: 30px;">
-                        <p>200文字</p>
+
+                        </div>
+                        <div class="col-md-6 usiro_ys">
+                        </div>
+                    </div><br>
+                    <input type="text" class="form-control nameupdate_ymn" name="username" required value=""
+                        placeholder="名前">
+
+                    <textarea class="sayu_ymn form-control alert-light" name="introduction" id="txt1" maxlength="200"
+                        placeholder="自己紹介"></textarea><br><br>
+                </div>
+                <div class="row">
+                    <div class="col-6" id="btn_nh">
+                        <button class="profileup_ymn" onclick="location.href='03_ホーム.html'"
+                            class="btn container-fluid color_white_yamani">キャンセル</button>
                     </div>
-                    <hr class="line" id="hr_nh">
-                    <div class="text-line " style="margin-bottom: 20px;">
-                    <p>好きなジャンル</p>
+                    <div class="col-6" id="btn_nh">
+                        <button class="noprofileup_ymn" value="保存" name="update" type="submit"
+                            class="btn container-fluid color_white_yamani">保存</button>
+                    </div>
                 </div>
             </div>
-            <div class="row magin40_yamanisi">
-                <div class="col-md-6" id="btn_nh">
-                <!--<div class="col-md-3" id="btn_nh">-->
-                    <button onclick="location.href='05_プロフィール画面.php'" class="btn container-fluid color_white_yamani" style="background-color:#FBA8B8;width: 90%;margin-left: 30px;">キャンセル</button>
-                </div>
-                <div class="col-md-6" id="btn_nh">
-                    <button value="保存" name="update" type="submit" class="btn container-fluid color_white_yamani" style="background-color:#FBA8B8;width: 90%;margin-right: 30px;">保存</button>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3" style="background-color:#FBA8B8;"></div>
-        </div>
-      </div>
     </form>
 
+
 </body>
+
 </html>
