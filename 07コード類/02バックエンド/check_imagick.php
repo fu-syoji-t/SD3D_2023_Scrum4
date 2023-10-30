387ecdbc3c52e01143b3e226aa25b4d7e44f32df
sota94 <2101212@s.asojuku.ac.jp>
2023-10-30 13:53:11 +0900

    更新

--- 07コード類/02バックエンド/check_imagick.php
+++ 07コード類/02バックエンド/check_imagick.php
@@ -1,7 +1,18 @@
 <?php
-if (extension_loaded('imagick')) {
-    echo "Imagickライブラリはインストールされています。";
-} else {
-    echo "Imagickライブラリはインストールされていません。";
-}
+//Zip形式で圧縮するために必要なモジュールを一度だけ読み込む
+require_once("Archive/Zip.php");
+
+//Archive_Zipインスタンスを作成する
+$zip = new Archive_Zip("test.zip");
+
+//圧縮するファイルを指定する
+//例では2つのファイル「text01.txt」「pict01.jpg」を指定
+//ファイルはこのプログラムと同じフォルダ内に置く
+$list = array("text01.txt","pict01.jpg");
+
+//圧縮作業をおこなう
+$zip->create($list);
+
+//メッセージの表示
+print("zip形式で圧縮しました。");
 ?>
