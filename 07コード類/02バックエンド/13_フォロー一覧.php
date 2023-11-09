138169ca2f672a3d1102d690dfc5ff6048f50707
ymnrio <2101213@s.asojuku.ac.jp>
2023-11-09 09:27:04 +0900

    更新

--- 07コード類/02バックエンド/13_フォロー一覧.php
+++ 07コード類/02バックエンド/13_フォロー一覧.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-    <title>やますたぐらむ | フォロワー一覧</title>
+    <title>やますたぐらむ | フォロー一覧</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -23,7 +23,7 @@
 <body class="body_ymn">
     <header class="header_ymn">
         <button type="button" class="chatback_ymn" onclick="location.href='06_プロフィール.html'" value="遷移">く</button>
-        <h5 class="dmname_ymn">フォロワー</h5>
+        <h5 class="dmname_ymn">フォロー</h5>
     </header>
 
     <?php
@@ -31,7 +31,7 @@
     session_start();
     $pdo = new PDO('mysql:host=localhost;dbname=yamasutagourmet;charset=utf8', 'root', 'root');
 
-    $sql = "SELECT * FROM follow WHERE partner_id = ? ORDER BY follow_id";
+    $sql = "SELECT * FROM follow WHERE user_id = ? ORDER BY follow_id";
     $ps = $pdo->prepare($sql);
     $ps->bindValue(1, $_SESSION['user']['id'], PDO::PARAM_INT);
     $ps->execute();
@@ -40,7 +40,7 @@
     foreach ($searchArray as $row) {
         $sql2 = "SELECT * FROM user WHERE user_id = ?";
         $ps2 = $pdo->prepare($sql2);
-        $ps2->bindValue(1, $row['user_id'], PDO::PARAM_INT);
+        $ps2->bindValue(1, $row['partner_id'], PDO::PARAM_INT);
         $ps2->execute();
         $searchArray2 = $ps2->fetchAll();
 
@@ -48,47 +48,23 @@
             $partnername = $row2['user_name'];
             $iconmedia = $row2['icon'];
 
-            $sql3 = "SELECT *, count(*) FROM follow WHERE user_id = ? AND partner_id = ? ORDER BY follow_id";
-            $ps3 = $pdo->prepare($sql3);
-            $ps3->bindValue(1, $_SESSION['user']['id'], PDO::PARAM_INT);
-            $ps3->bindValue(2, $row['user_id'], PDO::PARAM_INT);
-            $ps3->execute();
-            $searchArray3 = $ps3->fetchAll();
-
-            foreach ($searchArray3 as $row3) {
-                if ($row3['count(*)'] == 1) {
-
-                    echo '<div class="left_ymn" id="icon_circle_nh"></div>
-                        <div class="ffname_ymn left_ymn">
-                        <h6 class="ffname_ymn">' . $partnername . '</h6>
-                        </div>
-                        <div class="right_ymn">
-                        <form action="ffupdate.php" method="post">
-                        <button type="hidden" name="followbtn[]" value="1,20" class="followbtn_ymn">フォローをやめる</button>
-                        </form>
-                        </div>
-                        <br><br>
-                        <p class="ffborder_ymn"></p>
-                        <br>';
-                } else {
-                    echo '<div class="left_ymn" id="icon_circle_nh"></div>
-                        <div class="ffname_ymn left_ymn">
-                            <h6 class="ffname_ymn">' . $partnername . '</h6>
-                        </div>
-                        <div class="right_ymn">
-                        <form action="ffupdate.php" method="post">
-                        <button type="hidden" name="follownbtn[]" value="2,20" class="nofollowbtn_ymn">フォローする</button>
-                        </form>
-                        </div>
-                        <br><br>
-                        <p class="ffborder_ymn"></p>
-                        <br>';
-                }
-            }
+            echo '<div class="left_ymn" id="icon_circle_nh"></div>
+                      <div class="ffname_ymn left_ymn">
+                      <h6 class="ffname_ymn">' . $partnername . '</h6>
+                      </div>
+                      <div class="right_ymn">
+                      <form action="ffupdate.php" method="post">
+                      <button type="hidden" name="followbtn[]" value="1,10" class="followbtn_ymn">フォローをやめる</a>
+                      </form>
+                      </div>
+                      <br><br>
+                      <p class="ffborder_ymn"></p>
+                      <br>';
         }
     }
 
     ?>
+    
 
     <!--↓↓↓メニューバー-->
     <div class="menu">
