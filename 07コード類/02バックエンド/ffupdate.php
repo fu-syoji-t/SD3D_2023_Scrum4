69b87c116ad0429d9f80136fdaae98a249923635
sota94 <2101212@s.asojuku.ac.jp>
2023-11-16 10:34:01 +0900

    更新

--- 07コード類/02バックエンド/ffupdate.php
+++ 07コード類/02バックエンド/ffupdate.php
@@ -5,15 +5,37 @@ session_start();
 require 'DBManager_ys.php';
 $dbmng = new DBManager();
 
-//フォローから来た時
-if($_POST['followbtn'][0] = 13){
+//受け取った値をそれぞれの変数に入れる
+$date = $_POST['followbtn'];
+$ffArray = explode(",", $date);
 
+$check = $ffArray[0]; //13か14なのか
+$partner_id = $ffArray[1]; //相手のuser_id
+
+//フォロワーの時に使う　1フォローする　2フォローをやめる
+if(isset($ffArray[2])){
+$ff_wakeru = $ffArray[2];
+}
+
+//フォローから来た時
+if($check == 13){
     //削除するsql
-    $ps = $dbmng->ff_delete($_SESSION['user']['id'],$_POST['followbtn'][1]);
+    $ps = $dbmng->ff_delete($_SESSION['user']['id'],$partner_id);
 
 //フォロワーから来た時
-}else if($_POST['followbtn'][0] = 14){
+}else if($check == 14){
 
+    //フォローするのかやめるのかを判別 フォローする場合
+    if($ff_wakeru == 1){
+
+        //フォローするsql
+        $ps = $dbmng->ff_insert($_SESSION['user']['id'],$partner_id);
+
+    }else if($ff_wakeru == 2){
+        
+        //フォローをやめる
+        $ps = $dbmng->ff_delete($_SESSION['user']['id'],$partner_id);
+    }
 }
 
 
