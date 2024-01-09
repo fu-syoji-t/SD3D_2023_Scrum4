bfe8bdc34447fd3c732134f95cb4eceb55781ed5
Hinochanco <2101181@s.asojuku.ac.jp>
2024-01-09 09:51:25 +0900

    更新

--- 07コード類/03_バックエンド最新/04_投稿詳細.php
+++ 07コード類/03_バックエンド最新/04_投稿詳細.php
@@ -31,7 +31,9 @@ foreach (glob($folderPath) as $file) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
     <!--↓画像のスライドショーの時のみ-->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
-    <link rel="stylesheet" type="text/css" href="../01フロントエンド/css/detail/slide_show.css">
+    <link rel="stylesheet" type="text/css" href="css/detail/slide_show.css">
+
+    
 </head>
 <style>
 </style>
@@ -225,6 +227,7 @@ foreach (glob($folderPath) as $file) {
     <div class="posttext_nh"><?php echo $postcontents; ?></div>
 
     <!--ハッシュタグを表示-->
+    <div class="posttag_nh">
     <?php
     $ps = $dbmng->tag_search($id);
     if (isset($ps)) {
@@ -236,6 +239,7 @@ foreach (glob($folderPath) as $file) {
         echo '</form>';
     }
     ?>
+    </div>
 
     <?php
 
@@ -307,7 +311,7 @@ foreach (glob($folderPath) as $file) {
 
     <div id="wrapper_ymn">
         <div class="menu_ymn">
-            <p class="border_ymn" style="margin-bottom: 10px;"></p>
+            <p class="border_nh" style="margin-bottom: 10px;"></p>
             <div class="row footer_ymn" style="padding-left:35px;">
                 <div class="row">
                     <div class="col-9">
@@ -352,9 +356,65 @@ foreach (glob($folderPath) as $file) {
     </div>
 
 </body>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+<!--自作のJS-->
+<script src="js/slide_show.js"></script>
+
+</html>
+ <!--メニューバー-->
+ <div id="wrapper_ymn">
+
+<div class="menu_ymn">
+    <p class="border_ymn" style="margin: 0px;"></p>
+    <div class="row footer_ymn" style="padding-left:35px;">
+        <div class="col-2" style="padding:0px;text-align: center;">
+            <form action="03_ホーム.php" method="post">
+                <button name="homelogo" type="hidden" value="1"
+                    style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                    <img src="img/やますたぐるめ_ホームロゴ.png" width="78">
+                </button>
+            </form>
+        </div>
+        <div class="col-2" style="padding:0px;text-align: center;">
+            <form action="09_検索.php" method="post">
+                <button name="homelogo" type="hidden" value="1"
+                    style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                    <img src="img/やますたぐるめ_検索ロゴ.png" width="78">
+                </button>
+            </form>
+        </div>
+        <div class="col-2" style="padding:0px;text-align: center;">
+            <form action="05_新規投稿作成.php" method="post">
+                <button name="homelogo" type="hidden" value="1"
+                    style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                    <img src="img/やますたぐるめ_新規投稿ロゴ.png" width="78">
+                </button>
+            </form>
+        </div>
+        <div class="col-2" style="padding:0px;text-align: center;">
+            <form action="11_メッセージ一覧.php" method="post">
+                <button name="homelogo" type="hidden" value="1"
+                    style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                    <img src="img/やますたぐるめ_.DMロゴ.png" width="78">
+                </button>
+            </form>
+        </div>
+        <div class="col-2" style="padding:0px;text-align: center;">
+            <form action="06_プロフィール.php" method="post">
+                <button name="homelogo" type="hidden" value="1"
+                    style="text-decoration: none; background-color: transparent; border: none; outline: none; box-shadow: none;">
+                    <img src="img/やますたぐるめ_プロフィールロゴ.png" width="78">
+                </button>
+            </form>
+        </div>
+        <div class="col-1" style="padding:0px"></div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <!--自作のJS-->
 <script src="../01フロントエンド/js/slide_show.js"></script>
+</body>
 
 </html>
